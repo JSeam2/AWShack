c680b9623c22fb3d8fc3c0463fc9bfa1b2426939
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-06-27 09:11:19 +0800

    basic code up

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ from flask import Flask, request, session, redirect, url_for, abort
 app = Flask(__name__)
 
 
-@app
+@app.route("/")
+def index():
+    return "Hello"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
